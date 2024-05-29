d7c7b19faf72633577776549899d741730492f35
luk1337 <priv.luk@gmail.com>
2024-05-29 17:30:57 +0530

    mt6877:sensors: Import sensors/2.0/multihal

--- ossi.mk
+++ ossi.mk
@@ -15,5 +15,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.mt6877
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.1.vendor \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.0-service-multihal.mt6877 \
+    libsensorndkbridge
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/oplus/ivan/ivan-vendor.mk)
