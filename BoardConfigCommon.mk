e967c2d3f49007e0628db74eb8d5842428f6c71c
Notganesh <chimeports@gmail.com>
2024-05-29 17:12:23 +0530

    mt6877:defined Architecture

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,5 +16,18 @@
 
 COMMON_PATH := device/oplus/mt6877-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT ?= armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76
+
 # inherit from the proprietary version
-#include vendor/oplus/mt6877-common/BoardConfigVendor.mk
+include vendor/oplus/mt6877-common/BoardConfigVendor.mk
