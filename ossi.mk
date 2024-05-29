a8de2d5a7a48f74616fdcf47f65048f3996a01e3
ManshuTyagi <himanshut567@gmail.com>
2024-05-29 17:24:09 +0530

    mt6877:imported common light hidl

--- ossi.mk
+++ ossi.mk
@@ -11,5 +11,9 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.mt6877
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/oplus/ivan/ivan-vendor.mk)
