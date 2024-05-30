5af2e63346f34cdac7e7b0b5afabc1d76effe815
techyminati <techyminati@outlook.com>
2024-05-30 17:23:28 +0530

    mt6877:Build HIDL hals from source

--- ossi.mk
+++ ossi.mk
@@ -145,6 +145,15 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhardware \
+    libhardware.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # IMS
 PRODUCT_BOOT_JARS += \
     mediatek-common \
