b50f8c6a4d20659d0e009cf609397a2597b11ead
SagarMakhar <SagarMakhar@gmail.com>
2024-05-30 15:46:15 +0530

    mt6877:ODM_MANIFEST_SKUS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,6 +154,9 @@ SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(DEVICE_PATH)/touch/include
 # VINTF
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+ODM_MANIFEST_SKUS += nfc
+ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/configs/vintf/manifest_nfc.xml
+ODM_MANIFEST_FILES := $(COMMON_PATH)/configs/vintf/manifest_odm.xml
 
 # inherit from the proprietary version
 #include vendor/oplus/mt6877-common/BoardConfigVendor.mk
