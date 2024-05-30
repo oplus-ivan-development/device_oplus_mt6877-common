12583e16d1a7b3974fe4ee0a0336896a1221127e
Notganesh <chimeports@gmail.com>
2024-05-30 17:32:21 +0530

    mt6877:build USB from source

--- ossi.mk
+++ ossi.mk
@@ -247,6 +247,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/nfc_features.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc/nfc_features.xml
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0.vendor \
+    android.hardware.usb@1.1.vendor \
+    android.hardware.usb@1.2.vendor \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.0.vendor \
+    android.hardware.usb.gadget@1.1.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
