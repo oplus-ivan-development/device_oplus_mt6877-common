69e8f4d4516c0cbe90e410a0f362a37423915ce1
luca020400 <luca.stefani.ge1@gmail.com>
2024-05-30 08:19:11 +0530

    mt6877:Enable updateable APEX

--- ossi.mk
+++ ossi.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
