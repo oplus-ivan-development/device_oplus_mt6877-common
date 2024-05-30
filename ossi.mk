ab66d0ec436cd797524b7f86aa76d2dc318e3fee
electimon <electimon@gmail.com>
2024-05-30 17:20:15 +0530

    mt6877:Build GNSS hals from source

--- ossi.mk
+++ ossi.mk
@@ -122,6 +122,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OplusDoze
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
 # IMS
 PRODUCT_BOOT_JARS += \
     mediatek-common \
