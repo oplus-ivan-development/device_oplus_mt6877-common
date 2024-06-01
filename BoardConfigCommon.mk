56c1a594ce7ba507bee8a8b1ac74d78f033f55fa
bengris32 <bengris32@protonmail.ch>
2024-06-01 07:35:39 +0530

    mt6877:setup UDFPS Overlays

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -181,7 +181,10 @@ SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(COMMON_PATH)/touch/include
 # VINTF
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/configs/vintf/manifest_nfc.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/configs/vintf/manifest_odm.xml
