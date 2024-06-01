4f76e1acbb0f2994adfe60b1883581866733f837
Notganesh <chimeports@gmail.com>
2024-06-01 08:10:24 +0530

    mt6877:updated vendor links

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,18 +7,16 @@ git clone https://github.com/oneplus-mediatek-dev/Kernel_oneplus_ivan.git -b mai
 git clone https://github.com/oneplus-mediatek-dev/device_oneplus_ivan.git -b main device/oplus/ivan
 
 # Vendor
-git clone https://gitlab.com/Ko_Hi/mt6877_vendor.git vendor/oplus/mt6877-common
-git clone https://gitlab.com/Ko_Hi/mt6877.git vendor/oplus/ivan
+git clone https://github.com/oneplus-mediatek-dev/vendor_oneplus_mt6877_common.git vendor/oplus/mt6877-common
+git clone https://github.com/oneplus-mediatek-dev/vendor_oplus_ivan.git vendor/oplus/ivan
 
 # Hardware
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
 git clone -b lineage-20 https://github.com/oplus-ossi-development/android_hardware_mediatek.git hardware/mediatek
+git clone -b lineage-20 https://github.com/oneplus-mediatek-dev/android_hardware_mediatek_wlan.git hardware/mediatek/wlan
 
 # Sepolicy
-git clone -b lineage-20 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+git clone -b lineage-20 https://github.com/nashc-dev/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
 
 # clang
 git clone --depth=1 https://github.com/oneplus-mediatek-dev/android_prebuilts_clang_host_linux-x86_clang-6443078.git -b 11.0.1 prebuilts/clang/host/linux-x86/clang-r383902
-
-# Source patches
-# bash device/oplus/mt6877-common/patches/apply.sh
