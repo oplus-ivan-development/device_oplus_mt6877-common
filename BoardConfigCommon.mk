af3e528681cfdc2ff1d5ab0bf6ab5f43d8b8d181
Giovix92 <giovix92@giovix92.com>
2024-06-04 08:51:53 +0530

    mt6877:props: Import missing crypto props

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,6 +151,7 @@ BOARD_SUPER_PARTITION_GROUPS := main
 # Properties
 TARGET_VENDOR_PROP += $(COMMON_PATH)/configs/props/vendor.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/configs/props/system.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/configs/props/product.prop
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := /proc/touchpanel/double_tap_enable
