7a5219daadfd9d8d8a3e3ac189fd7322fa5fb81a
ManshuTyagi <himanshut567@gmail.com>
2024-06-27 15:46:56 +0000

    mt6877:build wifi 1.0 service

--- ossi.mk
+++ ossi.mk
@@ -460,7 +460,6 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0.vendor \
     android.hardware.wifi@1.1.vendor \
     android.hardware.wifi@1.2.vendor \
     android.hardware.wifi@1.3.vendor \
@@ -469,7 +468,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi.hostapd@1.0.vendor \
     android.hardware.wifi.hostapd@1.1.vendor \
     android.hardware.wifi.hostapd@1.2.vendor \
-    android.hardware.wifi@1.0-service-lazy \
     android.hardware.wifi.hostapd@1.3.vendor \
     android.hardware.wifi.supplicant@1.0.vendor \
     android.hardware.wifi.supplicant@1.1.vendor \
@@ -479,8 +477,7 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor \
-    hostapd \
-    hostapd_cli
+    android.hardware.wifi@1.0-service.ivan
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
