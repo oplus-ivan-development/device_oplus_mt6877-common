f191942e569cc8e9ccae9469d9bf2104ad6ff303
Notganesh <chimeports@gmail.com>
2024-12-05 20:00:13 +0530

    mt6877:overrlay:dropped duplicate configs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# Kernel
 git clone https://github.com/Notganesh/kernel_oneplus_ivan.git -b vooc kernel/oplus/mt6877
 
+# Device Tree
 git clone https://github.com/oplus-ivan-development/android_device_oplus_ivan.git device/oplus/ivan
 
+# Vendor Tree
 git clone https://gitlab.com/Ko_Hi/vendor_ivan.git vendor/oplus/ivan
 
+# Hardware
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
-
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
-
 git clone -b lineage-20 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
 
 # clang
