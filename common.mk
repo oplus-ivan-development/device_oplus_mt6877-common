06def637f03151689dfa88c38da1fb2879af3d57
erfanoabdi <erfangplus@gmail.com>
2024-12-05 08:12:42 +0530

    mt6877:Uprev Wi-Fi service to 1.6

--- common.mk
+++ common.mk
@@ -48,12 +48,7 @@ PRODUCT_PACKAGES += \
     libdynproc \
     libhapticgenerator \
     audio.r_submix.default \
-    audio.usb.default \
-
-PRODUCT_PACKAGES += \
-    MtkInCallService \
-    BesLoudness \
-    PowerOffAlarm
+    audio.usb.default
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
@@ -451,15 +446,14 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.5.vendor \
+    android.hardware.wifi.hostapd@1.3.vendor \
+    android.hardware.wifi.supplicant@1.4.vendor \
     android.hardware.tetheroffload.config@1.0.vendor \
-    android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi-service \
-    hostapd \
-    libwifi-hal-wrapper \
-    wpa_supplicant
+    android.hardware.wifi@1.0-service-lazy.mt6877
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
