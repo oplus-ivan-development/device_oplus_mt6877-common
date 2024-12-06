74be54235ad8f18bd969b9430fbb1940bfaaaeaa
Himanshu Tyagi <himanshut567@gmail.com>
2024-12-06 18:26:47 +0530

    mt6877:common:Update dalvik-heap device config

--- common.mk
+++ common.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Dalvik VM Configuration
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
