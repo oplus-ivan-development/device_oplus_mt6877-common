a813de2f6f5261ef16447b6f81a7dd04d291019d
Nishant Kumar <www.rajsonu13@gmail.com>
2024-12-07 09:31:23 +0530

    mt6877-common:Initial powerhint for MT6877

--- common.mk
+++ common.mk
@@ -373,7 +373,8 @@ PRODUCT_COPY_FILES += \
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Radio
 PRODUCT_PACKAGES += \
