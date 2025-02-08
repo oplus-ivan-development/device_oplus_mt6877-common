07f3c7ebaf04cf574a54a0860c561eb39b3da826
Mashopy <eliasgheeraert@gmail.com>
2025-02-08 15:59:38 +0530

    mt6877-common:Build Wi-Fi stack from source Build all components of the Wi-Fi stack from source -   Wi-Fi service, wpa_supplicant, hostapd, and libwifi-hal.

--- common.mk
+++ common.mk
@@ -466,14 +466,15 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.5.vendor \
-    android.hardware.wifi.hostapd@1.3.vendor \
-    android.hardware.wifi.supplicant@1.4.vendor \
     android.hardware.tetheroffload.config@1.0.vendor \
+    android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy.mt6877
+    android.hardware.wifi@1.0-service-lazy \
+    wpa_supplicant \
+    hostapd \
+    libwifi-hal-mt66xx
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,12 +7,12 @@ git clone https://github.com/Notganesh/kernel_oneplus_ivan.git -b vooc kernel/op
 git clone https://github.com/oplus-ivan-development/android_device_oplus_ivan.git device/oplus/ivan
 
 # Vendor Tree
-git clone https://gitlab.com/Ko_Hi/vendor_ivan.git vendor/oplus/ivan
+git clone https://github.com/oplus-ivan-development/android_vendor_oplus_ivan.git vendor/oplus/ivan
 
 # Hardware
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
 git clone -b lineage-20 https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
 git clone -b lineage-20 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
 
-# clang
-git clone --depth=1 https://github.com/oneplus-mediatek-dev/android_prebuilts_clang_host_linux-x86_clang-6443078.git -b 11.0.1 prebuilts/clang/host/linux-x86/clang-r383902
+# wlan
+git clonehttps://github.com/nashc-dev/android_hardware_mediatek_wlan.git -b lineage-20 hardware/mediatek/wlan
