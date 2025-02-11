fa8d57c3f2a1f3f4b5f772dbfc63fa2d5dfa74c0
Notganesh <chimeports@gmail.com>
2025-02-11 15:07:40 +0000

    mt6877-common:track clang from org

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Kernel
-git clone https://github.com/Notganesh/kernel_oneplus_ivan.git -b vooc kernel/oplus/mt6877
+git clone https://github.com/Notganesh/kernel_oneplus_mt6877.git -b 14 kernel/oplus/mt6877
 
 # Device Tree
 git clone https://github.com/oplus-ivan-development/android_device_oplus_ivan.git device/oplus/ivan
@@ -14,3 +14,6 @@ git clone https://github.com/oplus-ivan-development/android_vendor_oplus_ivan.gi
 git clone -b lineage-22.1 https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
 git clone -b lineage-22.1 https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
 git clone -b lineage-22.1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+
+# clang
+git clone --depth=1 https://github.com/Notganesh/android_prebuilts_clang_host_linux.git prebuilts/clang/host/linux-x86/clang-r383902
