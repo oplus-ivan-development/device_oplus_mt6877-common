15891eae7010a30535bafc2348c77a935c2f2d4b
Notganesh <chimeports@gmail.com>
2025-02-17 11:28:42 +0000

    mt6877-common:fixed vendor path

--- common.mk
+++ common.mk
@@ -598,4 +598,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += libshim
 
 # Inherit from vendor blobs
-$(call inherit-product, vendor/oplus/mt6877-common/mt6877-common-vendor.mk)
+$(call inherit-product, vendor/oneplus/mt6877-common/mt6877-common-vendor.mk)
