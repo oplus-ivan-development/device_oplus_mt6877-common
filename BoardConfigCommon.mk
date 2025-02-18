ed9c5d8eef71f05a7d6172a9930d94cf2ec5ddc1
subhagamer <lordkazuya1@gmail.com>
2025-02-18 23:32:14 +0530

    mt6877-common:Add device FCM for stock HALs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,18 +62,18 @@ TARGET_NO_BOOTLOADER := true
 
 BOARD_HAS_MTK_HARDWARE := true
 
-# Build system
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_PREBUILT_ELF_FILES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
-BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml
 
-# VINTF
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
-DEVICE_MATRIX_FILE += $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+# Compatibility matrix
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml
-ODM_MANIFEST_FILES := $(COMMON_PATH)/configs/vintf/manifest_odm.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
+
+# LOS FCM
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Display
 TARGET_SCREEN_DENSITY := 480
