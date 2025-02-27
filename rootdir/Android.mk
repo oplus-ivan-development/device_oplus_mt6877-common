938123914d1acfa8e81c1e7f820cfb502f94036f
Notganesh <chimeports@gmail.com>
2025-02-27 21:40:31 +0530

    mt6877-common:Rootdir:Update From F.58

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -139,3 +139,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.oplus.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_SYSTEM_EXT_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init_conninfra.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init_conninfra.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
